d2b1ae138bc30271a7afc1c6175d4b413fbf39ba
obaran <o.baranonur@gmail.com>
2023-12-28 00:27:06 +0100

    first commit

---
+++ src/main/java/com/etabli/reservation/EtabliReservationApplication.java
@@ -0,0 +1,13 @@
+package com.etabli.reservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtabliReservationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EtabliReservationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/etabli/reservation/EtabliReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.etabli.reservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EtabliReservationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
